17e4291082373df6acd86a00daf3e03cde7d3efc
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-08-16 00:12:09 +0100

    Upgrade to version `0.1.1`

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-amazon-sns-mobile-push-notification",
-    version="0.1",
+    version="0.1.1",
     packages=["amazon_sns_mobile_push_notification"],
     include_package_data=True,
     license="MIT License",
