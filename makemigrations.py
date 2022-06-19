e5c82fc630de05f44dd74e1210365b3b131fbda7
Ernesto F. González <ernestogonzalezz@yahoo.com>
2022-06-19 01:04:29 +0100

    Delete deprecated files

--- makemigrations.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from django.core.management import call_command
-
-from setup_django import setup_django
-
-setup_django()
-call_command("makemigrations", "django_amazon_sns_mobile_push_notification")
