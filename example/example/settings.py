0667eab06f730f77acfbc0ab45f877afc357a3e7
Ernesto F. González <ernestogonzalezz@yahoo.com>
2022-06-20 16:17:46 +0100

    Install app in example project

--- example/example/settings.py
+++ example/example/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_amazon_sns_mobile_push_notification",
 ]
 
 MIDDLEWARE = [
